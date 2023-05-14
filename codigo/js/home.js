1fe202c60f1a2b61c7388bd28e0741e79bc0cb73
marcos <curymarcos@live.com>
2023-05-14 17:13:37 -0300

    Finish home_screen

--- codigo/js/home.js
+++ codigo/js/home.js
@@ -1,3 +1,5 @@
+// Functions to generate pub items
+
 function GenerateHTMLCodeStarsItem(n_stars) {
     const StartFull = `
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
@@ -15,7 +17,7 @@ function GenerateHTMLCodeStarsItem(n_stars) {
 
     let AllStars = "";
 
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         if (i <= n_stars) {
             AllStars += StartFull;
         } else {
@@ -44,7 +46,7 @@ function GenerateHTMLCodeItem(item) {
                 <div class="div-item-mobile-star">
                     ${divStar}
                 </div>
-                <p class="p-item-address">${item.address}</p>
+                <p class="p-item-address">${item.address.fully}</p>
                 <div class="div-item-coment">
                     <p class="p-item-coment-title">Comentário Selecionado:</p>
                     <p class="p-item-coment-text">${item.select_comment}</p>
@@ -56,7 +58,7 @@ function GenerateHTMLCodeItem(item) {
         </div>`;
 }
 
-function GenerateItems() {
+function GenerateItems(filters) {
     let listItems = document.getElementById("div-items");
     listItems.innerHTML = "";
 
@@ -64,21 +66,161 @@ function GenerateItems() {
     let exampleItemJSON = [
         {
             name: "Jundú Restaurante Lounger Bar",
-            address: "Avenida Leovigildo Dias Vieira, 810, Itaguá – Ubatuba, SP",
+            address: {
+                fully: "Avenida Leovigildo Dias Vieira, 810, Itaguá – Ubatuba, SP",
+                zone: "Itaguá"
+            },
             select_comment: "Bar execelente, bonito e possui ótimos drinks.",
             n_stars: 3,
-            n_reviews: 129
+            n_reviews: 129,
+            media_value: 70,
+            type_food: "Mexicano",
+            type_local: "Pub",
+            differential: [
+                "music",
+                "karaoke"
+            ]
         },
         {
             name: "Belli Belli Gastrobar",
-            address: "Avenida José Bento Ribeiro Dantas, Porto da Barra",
+            address: {
+                fully: "Avenida José Bento Ribeiro Dantas, Porto da Barra",
+                zone: "Porto da Barra"
+            },
             select_comment: "Bar com exelente vista.",
             n_stars: 2,
-            n_reviews: 297
+            n_reviews: 297,
+            media_value: 20,
+            type_food: "Espetos",
+            type_local: "Restaurante",
+            differential: [
+                "chop",
+                "sinuca"
+            ]
+        },
+        {
+            name: "Teste do marcos",
+            address: {
+                fully: "Rua Araguari 999, Coração Eucarístico",
+                zone: "Coração Eucarístico"
+            },
+            select_comment: "O restaurante é bem bonito e interessante.",
+            n_stars: 5,
+            n_reviews: 151,
+            media_value: 200,
+            type_food: "Italiano",
+            type_local: "Bar",
+            differential: [
+                "thematic",
+                "happyhour",
+                "music"
+            ]
         }
     ];
 
-    exampleItemJSON.forEach(item => {
-        listItems.innerHTML += GenerateHTMLCodeItem(item);
+    let exampleItemJSONFilter = exampleItemJSON;
+
+    if (filters?.type == "name") {
+        exampleItemJSONFilter = exampleItemJSONFilter.filter(item => item.name.toLowerCase().startsWith(filters.name.toLowerCase()));
+    } else if (filters?.type == "form") {
+        if (filters.form[0].value != "--") {
+            exampleItemJSONFilter = exampleItemJSONFilter.filter(item => item.address.zone == filters.form[0].value);
+        }
+        if (filters.form[1].value != "--") {
+            if (filters.form[1].value == "Por número de avaliações") {
+                exampleItemJSONFilter = exampleItemJSONFilter.sort((firstItem, secondItem) => {
+                    return secondItem.n_reviews - firstItem.n_reviews;
+                });
+            } else if (filters.form[1].value == "Melhores avaliações") {
+                exampleItemJSONFilter = exampleItemJSONFilter.sort((firstItem, secondItem) => {
+                    return secondItem.n_stars - firstItem.n_stars;
+                })
+            }
+        }
+        if (filters.form[2].value != "--") {
+            let values = filters.form[2].value.split(' ')[1].split('-');
+            exampleItemJSONFilter = exampleItemJSONFilter.filter(item => {
+                return item.media_value >= values[0] || values[1] <= item.media_value;
+            });
+        }
+        if (filters.form[3].value != "--") {
+            exampleItemJSONFilter = exampleItemJSONFilter.filter(item => {
+                return item.type_food == filters.form[3].value;
+            });
+        }
+        if (filters.form[4].value != "--") {
+            exampleItemJSONFilter = exampleItemJSONFilter.filter(item => {
+                return item.type_local == filters.form[4].value;
+            });
+        }
+
+        for (let i = 5; i <= 10; i++) {
+            if (filters.form[i].checked) {
+                exampleItemJSONFilter = exampleItemJSONFilter.filter(item => {
+                    return item.differential.includes(filters.form[i].value);
+                });
+            }
+        }
+    }
+
+    if (exampleItemJSONFilter.length != 0) {
+        exampleItemJSONFilter.forEach(item => {
+            listItems.innerHTML += GenerateHTMLCodeItem(item);
+        });
+    } else {
+        listItems.innerHTML = "<div id=\"div-item-none\"><p>Nenhum resultado foi encontrado</p></div>";
+    }
+
+
+}
+
+// Functions modal filter
+function modalFilterAparrence() {
+    let modalFilterStyle = document.getElementById("div-modal-filter").style;
+    if (modalFilterStyle.display == "" || modalFilterStyle.display == "none") {
+        modalFilterStyle.display = "flex";
+
+        // Use mock until database was not set
+        let zoneOptions = ["--", "Coração Eucarístico", "Padre Eustáquio", "Savassi"];
+        let orderOptions = ["--", "Por número de avaliações", "Melhores avaliações"];
+        let mediaValueOptions = ["--", "R$ 0-30", "R$ 31-50", "R$ 51-80", "R$ 81-100", "R$ 101-150", "R$ 151-..."];
+        let typeFoodOptions = ["--", "Mexicano", "Espetos", "Italiano"];
+        let typeLocals = ["--", "Restaurante", "Bar", "Pub"];
+
+        modalFilterOptions("select-modal-zone", zoneOptions);
+        modalFilterOptions("select-modal-order", orderOptions);
+        modalFilterOptions("select-modal-mediavalue", mediaValueOptions);
+        modalFilterOptions("select-modal-typefood", typeFoodOptions);
+        modalFilterOptions("select-modal-typelocal", typeLocals);
+    } else {
+        modalFilterStyle.display = "none";
+    }
+}
+
+function modalFilterOptions(elementId, options) {
+    let zoneSelect = document.getElementById(elementId);
+
+    let htmlOptions = "";
+
+    options.forEach(option => {
+        htmlOptions += "<option value='" + option + "'>" + option + "</option>";
+    });
+
+    zoneSelect.innerHTML = htmlOptions;
+}
+
+function filterByName() {
+    let textFilter = document.getElementById("input-filter-name-bar").value;
+    GenerateItems({
+        type: "name",
+        name: textFilter
+    });
+}
+
+function filterSubmit() {
+    document.getElementById("div-modal-filter").style.display = "none";
+    GenerateItems({
+        type: "form",
+        form: document.forms[0].elements
     });
 }
